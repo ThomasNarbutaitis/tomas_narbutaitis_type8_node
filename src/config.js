7f20fb9a314a93cc29891cad708f3e7fd427d8c0
Tomas <tomas_narba@yahoo.com>
2022-05-26 08:35:52 +0300

    express server

---
+++ src/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const PORT = process.env.SERVER_PORT || 5000;
+
+module.exports = {
+  PORT,
+};
--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('server.js');
+const express = require('express');
+const { PORT } = require('./config');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => console.log('Server is runnin on port', PORT));
